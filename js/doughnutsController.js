26deb1cca57c143b51b2d445d8b37769d946e791
Ambar Sethi <ambarrrsethi@gmail.com>
2015-10-13 20:03:30 +0100

    got the donut bloody deleting

--- js/doughnutsController.js
+++ js/doughnutsController.js
@@ -11,12 +11,10 @@ function DoughnutController($http){
   function getDoughnuts(){
     $http
       .get('http://api.doughnuts.ga/doughnuts')
-      .then(function(response, err){
-        if(err) console.log(err)
+      .then(function(response){
         self.all = response.data
       });
   };
-
   getDoughnuts()
 
   self.addDoughnut = addDoughnut;
@@ -25,10 +23,28 @@ function DoughnutController($http){
   function addDoughnut(){
     $http
       .post('http://api.doughnuts.ga/doughnuts', self.newDoughnut)
-      .then(function(response, err){
-        if(err) console.log(err)
-        console.log(response, self.newDoughnut)
+      .then(function(response){
+      self.all.push(response.data);
       });
+      self.newDoughnut = {};
+  };
+
+  self.deleteDoughnut = deleteDoughnut;
+  self.delDoughnut = {};
+
+  function deleteDoughnut(donut){
+    // console.log(donut)
+    $http
+      .delete('http://api.doughnuts.ga/doughnuts/' + donut)
+      .then(function(response){
+        var array = self.all
+        var item = donut
+        var index = array.indexOf(item);
+        array.splice(index, 1);
+
+        getDoughnuts();
+      });
+      self.delDoughnut = {};
   };
 
 
